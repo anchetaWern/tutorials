37c155676bb8666b3130f1a39e5f1d52015c3448
wern ancheta <ancheta.wern@gmail.com>
2012-08-01 16:34:37 +0800

    added mustache, store js and sugar js code samples

---
+++ mustache/data.php
@@ -0,0 +1,3 @@
+<?php
+$ninja = array('awesome'=>false, 'magic_word'=>'cool', 'name'=>'naruto', 'skills'=>array('rasenshuriken'=>array('super', 'golden', 'tsukasenmai'), 'rasengan'=>array('abc','def','ghi'), 'kage bunshin'=>array('kame', 'hame', 'wave')));
+echo json_encode($ninja);
---
+++ mustache/data2.php
@@ -0,0 +1,9 @@
+<?php
+$data2 = ['ninjas'=>[
+			'naruto'=>['name'=>'naruto', 'skills'=>['rasengan', 'kage bunshin']], 
+			'sasuke'=>['name'=>'sasuke', 'skills'=>['chidori', 'sharingan']],
+			'lee'=>['name'=>'lee', 'skills'=>['leaf hurricane', '5 gates']]
+		]
+		];
+		
+echo json_encode($data2);		
